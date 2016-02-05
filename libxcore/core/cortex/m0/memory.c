e9be02f3048cae0110bd0dc5dd5d15669e1f1bbe
xent <alexdmitv@gmail.com>
2016-02-06 00:35:51 +0300

    Fixed directory structure.

--- libxcore/core/cortex/m0/memory.c
+++ libxcore/core/cortex/m0/memory.c
@@ -1,10 +1,26 @@
 /*
  * memory.c
- * Copyright (C) 2016 xent
+ * Copyright (C) 2014 xent
  * Project is distributed under the terms of the GNU General Public License v3.0
  */
 
 #include <memory.h>
+/*----------------------------------------------------------------------------*/
+bool compareExchangePointer(void **pointer, const void *expected, void *desired)
+{
+  bool status = false;
+
+  __interruptsDisable();
+  /* Compare current state with expected state */
+  if (*pointer == expected)
+  {
+    *pointer = desired;
+    status = true;
+  }
+  __interruptsEnable();
+
+  return status;
+}
 /*----------------------------------------------------------------------------*/
 uint32_t countLeadingZeros32(uint32_t value)
 {
