fa125e1e5569891b24106d8fc5061598ed62a09a
xent <alexdmitv@gmail.com>
2024-01-21 00:42:26 +0300

    Updated build scripts.

--- toolchains/cortex-m0.cmake
+++ toolchains/cortex-m0.cmake
@@ -10,7 +10,7 @@ set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 set(CMAKE_SIZE "arm-none-eabi-size")
 
-set(FLAGS_CPU "-mthumb -nostartfiles -mcpu=cortex-m0")
+set(FLAGS_CPU "-nostartfiles -mthumb -mcpu=cortex-m0")
 
 # Disable linking stage because cross-compiling toolchain cannot link without custom linker script
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
--- toolchains/cortex-m4-fpu.cmake
+++ toolchains/cortex-m4-fpu.cmake
@@ -10,7 +10,7 @@ set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 set(CMAKE_SIZE "arm-none-eabi-size")
 
-set(FLAGS_CPU "-mthumb -nostartfiles -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+set(FLAGS_CPU "-nostartfiles -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16")
 
 # Disable linking stage because cross-compiling toolchain cannot link without custom linker script
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
