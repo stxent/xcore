139d2514f853baf998ce49692d5ac34a3a3f47e8
xent <alexdmitv@gmail.com>
2026-03-28 20:26:30 +0300

    E24 core toolchain file changed medlow model.

--- toolchains/risc-v-sifive-e24.cmake
+++ toolchains/risc-v-sifive-e24.cmake
@@ -11,7 +11,7 @@ set(CMAKE_C_COMPILER "riscv-none-elf-gcc")
 set(CMAKE_CXX_COMPILER "riscv-none-elf-g++")
 set(CMAKE_SIZE "riscv-none-elf-size")
 
-set(FLAGS_CPU "-nostartfiles -march=rv32imac_zicsr -mtune=sifive-e24 -mabi=ilp32 -mcmodel=medany")
+set(FLAGS_CPU "-nostartfiles -march=rv32imac_zicsr -mtune=sifive-e24 -mabi=ilp32 -mcmodel=medlow")
 
 # Disable linking stage because cross-compiling toolchain cannot link without custom linker script
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
